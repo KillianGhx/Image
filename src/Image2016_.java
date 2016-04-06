8997776cde73bb5651fda2db3c3236f37504afc1
jclerici <jclerici@graph-e.com>
2016-04-06 16:16:59 +0200

    Problème avec la création des composantes.

--- src/Image2016_.java
+++ src/Image2016_.java
@@ -6,18 +6,20 @@ import ij.gui.*;
 import ij.plugin.filter.*;
 import ij.process.*;
 
-/**
- * Rien a completer, juste a tester. (Encore faut-il que la methode Outils.convoluer(...) fonctionne.) Le masque de convolution est le suivant: | 0 1 0 | L4 = | 1 -4 1 | | 0 1 0 |
- */
 public class Image2016_ implements PlugInFilter {
 
 	public void run(ImageProcessor ip) {
+
 		// Sobel
 		ImagePlus imp = this.afficheMatrice(ip, "Sobel", true);
+		ImageProcessor ip2 = imp.getProcessor();
 
-		// Composantes Connexes 
-		HashMap<Integer, ArrayList<Integer[]>> composantes = composantes(ip);
-		
+		// Composantes Connexes
+		// TODO Pourquoi les composantes ne sont-elles pas enregistrées ?
+		HashMap<Integer, ArrayList<int[]>> composantes = getComposantes(ip2);
+
+		// Cadres
+		appliqueCadres(cadres(composantes, ip2), ip2);
 		// TODO Ajouter l'étude des composantes connexes
 
 		new ImageWindow(imp);
@@ -28,77 +30,178 @@ public class Image2016_ implements PlugInFilter {
 	}
 
 	/**
+	 * Applique les cadres trouvés pour chaque composante connexe afin de montrer que la reconnaissance de composantes fonctionne relativement correctement
+	 * 
+	 * @param cadres
+	 *            Les cadres à appliquer
+	 * @param ip
+	 */
+	public static void appliqueCadres(HashMap<Integer, int[]> cadres, ImageProcessor ip) {
+		int couleur = 255;
+
+		for(int i = 0; i < cadres.size(); i++){
+			// Constante des index du tableau des extrêmeités des cadres
+			int minX = 0, minY = 2, maxX = 3, maxY = 4;
+			int[] extremes = cadres.get(i);
+
+			// Coté gauche
+			for(int x = extremes[minX] - 100; x <= extremes[minX]; x++){
+				ip.drawLine(x, extremes[minY] - 100, x, extremes[maxY] + 100);
+			}
+
+			// Coté droit
+			for(int x = extremes[maxX]; x <= extremes[maxX] + 100; x++){
+				ip.drawLine(x, extremes[minY] - 100, x, extremes[maxY] + 100);
+			}
+
+			// Haut
+			for(int y = extremes[maxY]; y < extremes[maxY] + 100; y++){
+				ip.drawLine(extremes[minX], y, extremes[maxX], y);
+			}
+
+			// Bas
+			for(int y = extremes[minY]; y < extremes[minY] - 100; y++){
+				ip.drawLine(extremes[minX], y, extremes[maxX], y);
+			}
+		}
+
+		//TODO A retirer quand le débuggage des composantes sera fini
+		if(cadres.size() > 0)
+			couleur = 255;
+		else
+			couleur = 0;
+		int width = ip.getWidth(), height = ip.getHeight();
+
+		for(int i = 0; i < width / 2; i++){
+			for(int j = 0; j < height; j++)
+				ip.set(i, j, couleur);
+		}
+	}
+
+	/**
+	 * Créé et retourne des cadres rouges à afficher pour chaque composante conne de HashMap composantes
+	 * 
+	 * @param composantes
+	 *            Les composantes connexes
+	 * @return cadres Les extrêmités horizontales et verticales de chaques composantes sous la forme suivante
+	 * 
+	 *         HashMap( index => Integer[minX, minY, maxX, maxY] )
+	 */
+	public static HashMap<Integer, int[]> cadres(HashMap<Integer, ArrayList<int[]>> composantes, ImageProcessor ip) {
+		HashMap<Integer, int[]> cadres = new HashMap<Integer, int[]>();
+
+		// Pour chaque composante trouvée
+		for(int i = 1; i < composantes.size(); i++){
+			int minX = ip.getWidth() - 1, minY = ip.getHeight() - 1, maxX = 0, maxY = 0;
+			// Coordonnées des pixels compris dans la composantes
+			Iterator<int[]> iterator = composantes.get(i).iterator();
+
+			// Pour chaque pixel, on cherche les extrémités verticales et horizontales
+			while(iterator.hasNext()){
+				int[] next = iterator.next();
+
+				minX = min(next[0], minX);
+				maxX = max(next[0], maxX);
+				minY = min(next[1], minY);
+				maxY = max(next[1], maxY);
+			}
+			int[] extremes = {minX, minY, maxX, maxY};
+
+			cadres.put(i, extremes);
+		}
+
+		return cadres;
+	}
+
+	/**
 	 * Cherches les composantes connexes d'une image
 	 * 
 	 * @param ip
 	 *            L'image à étudier
+	 * @return composantes Les composantes connexes trouvées sous la forme suivante HashMap( index => ArrayList(Integer[x, y]) )
+	 * 
+	 *         index étant le numéro de la composante partant de 0 et s'incrémentant de 1 à chaque composante ajoutée x et y étant les coordonnées respectivement verticales et horizontales d'un pixel
 	 */
-	public static HashMap<Integer, ArrayList<Integer[]>> composantes(ImageProcessor ip) {
+	public static HashMap<Integer, ArrayList<int[]>> getComposantes(ImageProcessor ip) {
 
 		// Numéro de la composante -> liste des pixels qu'elle inclut
-		HashMap<Integer, ArrayList<Integer[]>> composantes = new HashMap<Integer, ArrayList<Integer[]>>();
-		int couleur = 255, height = ip.getHeight(), width = ip.getWidth();
+		HashMap<Integer, ArrayList<int[]>> composantes = new HashMap<Integer, ArrayList<int[]>>();
+		int couleur = 90, height = ip.getHeight(), width = ip.getWidth();
 
 		// Parcours des pixels
 		for(int y = 0; y < height; y++){
 			for(int x = 0; x < width; x++){
 
 				// Si le pixel est de la couleur du contour et qu'il n'est pas dans les composantes
-				if(ip.getPixel(x, y) == couleur && !isVisited(composantes, x, y)){
+				if(ip.getPixel(x, y) > couleur && !isVisited(composantes, x, y)){
 					int composanteKey = -1;
-					ArrayList<Integer[]> toAdd = new ArrayList<Integer[]>();
-					Integer[] pixel = {x, y};
+					ArrayList<int[]> toAdd = new ArrayList<int[]>();
+					int[] pixel = {x, y};
 					toAdd.add(pixel);
 
-					// TODO Gérer la rencontre de plusieurs composantes en (x,y)
-					// On regarde les pixels adjacents pour les ajouter à une éventuelle composante
-					// et pour ne pas en créer une de trop
-					for(int i=x-1;i<x+2;x++){
-						for(int j=y-1;j<y+2;y++){
-							if(ip.getPixel(i, j) == couleur){
+					/*
+					 * On vérifie si les pixels autour sont dans les bonnes couleurs et s'ils font déjà parti d'une composante Voisin dans une composante ? Si le pixel sélectionné n'a pas de composante on le place dans celle-ci autrement on ajoute celle du voisin dans la composante provisoire du pixel sélectionné Voisin pas dans une composante ? On passe au suivant
+					 */
+					for(int i = x - 1; i < x + 2; x++){
+						for(int j = y - 1; j < y + 2; y++){
+
+							// Le pixel voisin est dans les bonnes couleurs
+							if(ip.getPixel(i, j) > couleur){
+
+								// Si le voisin du pixel sélectionné fait parti d'une composante
 								if(isVisited(composantes, i, j)){
-									// S'il y a déjà une autre composante d'identifiée c'est que plusieurs se rejoignent : une fusion s'impose
-									if(composanteKey > -1 && composanteKey != getComposanteKey(composantes, i, j)){
+									// Si le pixel sélectionné ne fait pas parti de composante, on le place dans celle du voisin
+									if(composanteKey == -1){
+										composanteKey = getComposanteKey(composantes, i, j);
+									}
+									// Si le pixel sélectionné a été ajouté à une composante et qu'un voisin fait déjà parti
+									// d'une autre composante c'est que plusieurs se rejoignent : une fusion s'impose
+									else if(composanteKey != getComposanteKey(composantes, i, j)){
 										int composanteKey2 = getComposanteKey(composantes, i, j);
-										ArrayList<Integer[]> insertInto = composantes.get(composanteKey);
-										Iterator<Integer[]> iterator = composantes.get(composanteKey2).iterator();
-										
+										ArrayList<int[]> insertInto = composantes.get(composanteKey);
+										Iterator<int[]> iterator = composantes.get(composanteKey2).iterator();
+
 										while(iterator.hasNext()){
 											insertInto.add(iterator.next());
 										}
-										
+
 										composantes.put(composanteKey, insertInto);
-										int k=composanteKey2;
-										
-										for(;k<composantes.size()-1;k++){
-											composantes.put(k, composantes.get(k+1));
+										int k = composanteKey2;
+
+										for(; k < composantes.size() - 1; k++){
+											composantes.put(k, composantes.get(k + 1));
 										}
-										
+
 										composantes.remove(k);
-									}else{
-										composanteKey = getComposanteKey(composantes, i, j);
 									}
-								}else{
-									Integer[] pixel2 = {i, j};
+								}else{ // Il n'en fait pas parti, donc on le rajoute à la composante en devenir
+									int[] pixel2 = {i, j};
 									toAdd.add(pixel2);
 								}
 							}
 						}
 					}
 					
-					Iterator<Integer[]> iterator = toAdd.iterator();
-					ArrayList<Integer[]> insertInto = composantes.get(composanteKey);
-					
+					// Si aucune composante n'a été sélectionnée pendant l'exploration des voisins, on place la composante provisoire à la fin
+					if(composanteKey == -1)
+						composanteKey = composantes.size();
+
+					Iterator<int[]> iterator = toAdd.iterator();
+					ArrayList<int[]> insertInto = composantes.get(composanteKey);
+
+					 if(insertInto == null)
+						insertInto = new ArrayList<int[]>();
+
 					// On ajoute les pixels découverts dans la composante correspondantes
 					while(iterator.hasNext()){
 						insertInto.add(iterator.next());
 					}
-					
-					composantes.put(composanteKey, insertInto);
+
+					 composantes.put(composanteKey, insertInto);
 				}
 			}
 		}
-		
+
 		return composantes;
 	}
 
@@ -110,9 +213,9 @@ public class Image2016_ implements PlugInFilter {
 	 * @param y
 	 * @return int key le numéro de la composante
 	 */
-	public static int getComposanteKey(HashMap<Integer, ArrayList<Integer[]>> composantes, int x, int y) {
-		ArrayList<Integer[]> pixel = new ArrayList<Integer[]>();
-		Integer[] coordinates = {x, y};
+	public static int getComposanteKey(HashMap<Integer, ArrayList<int[]>> composantes, int x, int y) {
+		ArrayList<int[]> pixel = new ArrayList<int[]>();
+		int[] coordinates = {x, y};
 		int key = -1, i = 0;
 
 		pixel.add(coordinates);
@@ -134,17 +237,18 @@ public class Image2016_ implements PlugInFilter {
 	 * @param y
 	 * @return boolean visited true s'il est présent et false autrement
 	 */
-	public static boolean isVisited(HashMap<Integer, ArrayList<Integer[]>> composantes, int x, int y) {
+	public static boolean isVisited(HashMap<Integer, ArrayList<int[]>> composantes, int x, int y) {
 		boolean visited = false;
-		ArrayList<Integer[]> pixel = new ArrayList<Integer[]>();
-		Integer[] coordinates = {x, y};
 		int i = 0;
 
-		pixel.add(coordinates);
-
 		while(i < composantes.size() && !visited){
-			if(composantes.get(i).containsAll(pixel))
-				visited = true;
+			Iterator<int[]> iterator = composantes.get(i).iterator();
+
+			while(iterator.hasNext() && !visited){
+				int[] next = iterator.next();
+				if(next[0] == x && next[1] == y)
+					visited = true;
+			}
 			i++;
 		}
 
@@ -259,6 +363,34 @@ public class Image2016_ implements PlugInFilter {
 		}
 
 		return imp;
+	}
+
+	/**
+	 * Retourne le plus petit de a et b
+	 * 
+	 * @param a
+	 * @param b
+	 * @return le plus petit nombre de a et b
+	 */
+	public static int min(int a, int b) {
+		if(a > b)
+			return b;
+		else
+			return a;
+	}
+
+	/**
+	 * Retourne le plus grand de a et b
+	 * 
+	 * @param a
+	 * @param b
+	 * @return le plus grand nombre de a et b
+	 */
+	public static int max(int a, int b) {
+		if(a < b)
+			return b;
+		else
+			return a;
 	}
 
 	public class Masque {
